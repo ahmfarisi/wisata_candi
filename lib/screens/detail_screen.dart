6a08be148d626a25053d1d025c7cfaf3f6c8ec58
Ahmad Farisi <ahmfarisi@gmail.com>
2023-12-11 08:53:07 +0700

    Finish Detail Header, Move to Detail Info

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -10,7 +10,49 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [],
+        children: [
+          //DETAIL HEADER
+          //Image Utama
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    candi.imageAsset,
+                    width: double.infinity,
+                    height: 300,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              //Tombol Back Custom
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple[100]?.withOpacity(0.8),
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                      onPressed: () {}, icon: const Icon(Icons.arrow_back)),
+                ),
+              ),
+            ],
+          ),
+          //DETAIL INFO
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                //Info Atas
+                //Info Tengah
+                //Info Bawah
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
