18172343ef97120e60fbf49f6bbc02c5ba3d514f
Ahmad Farisi <ahmfarisi@gmail.com>
2023-12-18 09:33:09 +0700

    Profile Screen Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
+import 'package:wisata_candi/screens/profile_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -17,9 +18,8 @@ class MainApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DetailScreen(
-        candi: candiList[0],
-      ),
+      //home: DetailScreen(candi: candiList[0],),
+      home: ProfileScreen(),
     );
   }
 }
